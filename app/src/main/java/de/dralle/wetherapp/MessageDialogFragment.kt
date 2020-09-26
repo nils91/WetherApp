183176e7ca6686e79a313071f30aece11a817bf4
Nils Dralle <nils.91@googlemail.com>
2020-09-26 12:50:55 +0200

    ok button in message dialog

--- app/src/main/java/de/dralle/wetherapp/MessageDialogFragment.kt
+++ app/src/main/java/de/dralle/wetherapp/MessageDialogFragment.kt
@@ -1,7 +1,9 @@
 package de.dralle.wetherapp
 
 import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 
@@ -12,7 +14,9 @@ class MessageDialogFragment : DialogFragment() {
             val builder = AlertDialog.Builder(activity!!)
             builder.setTitle(R.string.need_gps_title)
             builder.setMessage(R.string.need_gps_reason)
-
+            builder.setNeutralButton(
+                R.string.gps_dialog_ok,
+                DialogInterface.OnClickListener { dialog, id -> Log.d(tag, "OK") })
             // Create the AlertDialog object and return it
             return builder.create()
         } else {
